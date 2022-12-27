9540d81e2cf597bbc3ef1ead857d5771563d79a5
jiito <benjamin.allanrahill@gmail.com>
2022-12-27 17:09:37 -0500

    fix: imports

--- homer/pages/index.tsx
+++ homer/pages/index.tsx
@@ -3,12 +3,13 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
 import { useSupabase } from "../utils";
-import { Auth, Login } from "../components/auth";
+import { Auth } from "../components/auth";
 import styled from "styled-components";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { supabase, session } = useSupabase();
   return (
     <>
       <Head>
